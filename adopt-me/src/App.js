97e456261d55680b14ecd467e491c53a94789452
GainiZh <gaini.zhulamanova@gmail.com>
2019-08-04 16:35:05 +0200

    Update App.js

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,10 +1,27 @@
+// create a component Pet
+const Pet = ({name, animal, breed}) => {
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, name),
+      React.createElement("h2", {}, animal),
+      React.createElement("h2", {}, breed)
+    ]);
+};
+
 // create a component
 const App = () => {
     // create a div and inside a h1
     return React.createElement(
         "div",
         { id: "something-important" },
-        React.createElement("h1", {}, "Adopt Me!")
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet,
+                 {name: "Markiza", animal: "Dog", breed: "Collie"}),
+            React.createElement(Pet,
+                 {name: "Gosha", animal: "Cat", breed: "Mixed"}),
+            React.createElement(Pet,
+                 {name: "Pepper", animal: "Bird", breed: "Parrot"})
+        ]
     );
 };
 
