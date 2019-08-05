c33e32ca9bcdb9226b76b24038aea177a902e63b
GainiZh <gaini.zhulamanova@gmail.com>
2019-08-05 20:29:40 +0200

    refactored

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,29 +1,17 @@
-// create a component Pet
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 // create a component
 const App = () => {
-  // create a div and inside a h1
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Markiza",
-      animal: "Dog",
-      breed: "Collie"
-    }),
-    React.createElement(Pet, { name: "Gosha", animal: "Cat", breed: "Mixed" }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Parrot"
-    })
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Markiza" animal="Dog" breed="Collie" />
+      <Pet name="Gosha" animal="Cat" breed="Mixed" />
+      <Pet name="Pepper" animal="Bird" breed="Parrot" />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
