48c672dd180ee7ec217a5ed50270e73d08fb3466
GainiZh <gaini.zhulamanova@gmail.com>
2019-08-11 12:41:09 +0200

    added new components

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 // create a component
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Markiza" animal="Dog" breed="Collie" />
-      <Pet name="Gosha" animal="Cat" breed="Mixed" />
-      <Pet name="Pepper" animal="Bird" breed="Parrot" />
+      <SearchParams />
     </div>
   );
 };
