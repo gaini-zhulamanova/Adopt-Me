3f3ca06dbfc7bac23050078b006c0230c356ec91
GainiZh <gaini.zhulamanova@gmail.com>
2019-08-18 17:12:28 +0200

    reformated pet component

--- adopt-me/src/Pet.js
+++ adopt-me/src/Pet.js
@@ -1,13 +1,21 @@
 import React from "react";
 // create a component Pet
-const Pet = props => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
-  );
-};
 
-export default Pet;
+export default function Pet({ name, animal, breed, media, location, id }) {
+  let hero = "http://placecorgi.com/300/300";
+  if (media.length) {
+    hero = media[0].small;
+  }
+
+  return (
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} als={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
+  );
+}
